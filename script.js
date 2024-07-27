2635c03560e92e10ba97220774e22561b3c9d19f
Karan <karan.l.k123321@gmail.com>
2024-07-27 21:23:14 +0530

    Update synchronization of icons pause and play

--- script.js
+++ script.js
@@ -1,95 +1,110 @@
+let songIndex = 0;
 let audioElement = new Audio("./assets/songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
-let myProgreassBar = document.getElementById("myProgressBar");
+let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("playingGif");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
   {
     songName: "abc",
-    filePath: "./assets/songs/song/1.mp3",
+    filePath: "./assets/songs/1.mp3",
     coverPath: "./assets/covers/1.jpg",
   },
   {
     songName: "def",
-    filePath: "./assets/songs/song/2.mp3",
+    filePath: "./assets/songs/2.mp3",
     coverPath: "./assets/covers/2.jpg",
   },
   {
     songName: "ghi",
-    filePath: "./assets/songs/song/3.mp3",
+    filePath: "./assets/songs/3.mp3",
     coverPath: "./assets/covers/3.jpg",
   },
   {
     songName: "jkl",
-    filePath: "./assets/songs/song/4.mp3",
+    filePath: "./assets/songs/4.mp3",
     coverPath: "./assets/covers/4.jpg",
   },
   {
     songName: "mno",
-    filePath: "./assets/songs/song/5.mp3",
+    filePath: "./assets/songs/5.mp3",
     coverPath: "./assets/covers/5.jpg",
   },
 ];
 
-//aranging all songs
-
+// Arranging all songs
 songItems.forEach((element, i) => {
   element.getElementsByTagName("img")[0].src = songs[i].coverPath;
-
   element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 });
 
+// Master play button
 masterPlay.addEventListener("click", () => {
-  //console.log("Clicked It !");
-
   if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
     masterPlay.classList.remove("fa-circle-play");
     masterPlay.classList.add("fa-circle-pause");
     gif.style.opacity = 1;
+    setPlayButton(songIndex);
   } else {
     audioElement.pause();
-
     masterPlay.classList.remove("fa-circle-pause");
     masterPlay.classList.add("fa-circle-play");
     gif.style.opacity = 0;
+    makeAllPlays();
   }
 });
 
+// Update progress bar
 audioElement.addEventListener("timeupdate", () => {
-  // console.log("Time update working");
-
-  //update seekbar as song plays
-  prog = parseInt((audioElement.currentTime / audioElement.duration) * 100);
-  // console.log(prog);
-
+  let prog = parseInt((audioElement.currentTime / audioElement.duration) * 100);
   myProgressBar.value = prog;
 });
 
-myProgreassBar.addEventListener("change", () => {
+// Seek in audio
+myProgressBar.addEventListener("change", () => {
   audioElement.currentTime =
     (myProgressBar.value * audioElement.duration) / 100;
 });
 
+// Reset all play buttons to play state
 const makeAllPlays = () => {
   Array.from(document.getElementsByClassName("songItemPlay")).forEach((ele) => {
     ele.classList.remove("fa-pause");
-    // ele.classList.add("fa-play");
+    ele.classList.add("fa-play");
   });
 };
 
-//array of list of songs and click buttons
+// Set the play button for the currently playing song
+const setPlayButton = (index) => {
+  document.getElementById(index).classList.remove("fa-play");
+  document.getElementById(index).classList.add("fa-pause");
+};
 
+// Play specific song
 Array.from(document.getElementsByClassName("songItemPlay")).forEach((ele) => {
   ele.addEventListener("click", (e) => {
     makeAllPlays();
 
-    // index=parseInt(e.target.id);
+    songIndex = parseInt(e.target.id);
     e.target.classList.remove("fa-play");
     e.target.classList.add("fa-pause");
-    audioElement.src = "./assets/songs/1.mp3";
+
+    audioElement.src = songs[songIndex].filePath;
     audioElement.currentTime = 0;
     audioElement.play();
+
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+    gif.style.opacity = 1;
   });
+});
+
+// When audio ends, reset play buttons
+audioElement.addEventListener("ended", () => {
+  masterPlay.classList.remove("fa-circle-pause");
+  masterPlay.classList.add("fa-circle-play");
+  gif.style.opacity = 0;
+  makeAllPlays();
 });
